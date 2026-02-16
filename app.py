3f50ca3669e9ea5f510c7294fe2560107663ec59
yasmeenm9 <yasmeenmohammed346@gmail.com>
2026-02-16 22:39:55 +0530

    Update port for Render

--- app.py
+++ app.py
@@ -325,6 +325,10 @@ def delete_account():
 
 # --- Run App ---
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=5000, debug=True)
 #with app.app_context():
  #   db.create_all()
